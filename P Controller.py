41b72fc518942c7b95a13d245211440a0f673f0c
joonholee99 <joonholee99@gmail.com>
2021-05-24 20:48:12 +0900

    5/24 Twiddle is done

--- P Controller.py
+++ P Controller.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 class Robot(object):
     def __init__(self, length = 20.0):
         self.x = 0
@@ -61,24 +61,74 @@ robot = Robot()
 robot.set(0,1,0)
 robot.set_steering_drift(10/180*np.pi)
 
-def run(robot, tau_p, tau_d,tau_i, n=100, speed=1):
+def run(robot, params, n=100, speed=1):
     x_trajectory = []
     y_trajectory = []
     CTE_past = robot.y
+    iCTE = 0
     sum_error = 0
-    for i in range(n):
+    for i in range(n*2):
         CTE = robot.y - 0
         dCTE = CTE - CTE_past
-        sum_error += CTE
-        robot.move(steering = -tau_p*CTE - tau_d*dCTE - tau_i*sum_error, distance = speed)
-        print(robot.__repr__)
+        iCTE += CTE
+        robot.move(steering = -params[0]*CTE - params[1]*dCTE - params[2]*iCTE, distance = speed)
+        # print(robot.__repr__)
         x_trajectory.append(robot.x)
         y_trajectory.append(robot.y)
         CTE_past = CTE
+        if i >= n:
+            sum_error += CTE**2
 
-    return x_trajectory, y_trajectory
+    return x_trajectory, y_trajectory, sum_error/(n)
 
-x_trajectory, y_trajectory = run(robot, 0.2,3,0.004)
+def twiddle(tol = 0.001):
+    p = [0,0,0]
+    dp = [1,1,1]
+    robot = Robot()
+    robot.set(0,1,0)
+    robot.set_steering_drift(10/180*np.pi)
+    x,y,best_error = run(robot,p)
+    n=0
+    while sum(dp) > tol:
+        for i in range(3):
+            p[i] += dp[i]
+            robot = Robot()
+            robot.set(0,1,0)
+            robot.set_steering_drift(10/180*np.pi)
+            x,y,error = run(robot,p)
+
+            if error < best_error:
+                best_error = error
+                dp[i] *= 1.1
+            else:
+                p[i] -= 2*dp[i]
+                robot = Robot()
+                robot.set(0,1,0)
+                robot.set_steering_drift(10/180*np.pi)
+                x,y,other_error = run(robot,p)
+
+                if other_error < best_error:
+                    best_error = other_error
+                    dp[i] *= 1.1
+                else:
+                    p[i] += dp[i]
+                    dp[i] *= 0.9
+        n += 1
+        print(p)
+        print("Error is",best_error)
+        print("iter is",n)
+        print("dp is",dp)
+        print("--------------------------------")
+                
+    return p
+
+p = twiddle()
+
+robot = Robot()
+robot.set(0,1,0)
+robot.set_steering_drift(10/180*np.pi)
+x_trajectory, y_trajectory, best_error = run(robot, p)
+print(y_trajectory[-1])
 
 n = len(x_trajectory)
 fig, (ax1, ax2) = plt.subplots(2,1,figsize=(8,8))
